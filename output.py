d25da9268d410251f4dc732c0eff98860a04cf84
FunKuchen <45318896+FunKuchen@users.noreply.github.com>
2020-03-21 15:27:59 +0100

    Update output.py

--- output.py
+++ output.py
@@ -4,12 +4,8 @@ def main():
     FOUT = open("output.txt", "w")
     data = getData()
     for d in data:
-        str1 = '{"name": '+ data[1] + ', "address": "'+ data[3]+'", "zip": '+ data[4] + ', "city": "'+ data[5] + '", "lat": "'+ data[4] +'", "lon": "' + data[4] + '", "mon": "' + data[6] + '", "tue": "' + data[7] +'", "wed": "' + data[8] +'", "thu": "' + data[9]'}'    
-        FOUT.write(str1+'\n')
-
-
-
-
+        str1 = '{"name": ' +data[1] + ', "address": "'+ data[3]+'", "zip": '+ data[4] + ', "city": "'+ data[5] + '", "lat": "'+ data[4] +'", "lon": "' + data[4] + '", "mon": "' + data[6] + '", "tue": "' + data[7] +'", "wed": "' + data[8] +'", "thu": "' + data[9] +'", "fri": "' + data[10] +'", "sat": "' + data[11] +'", "sun": "' + data[12] +'", "url": "' + data[13] +'", "phone": "' + data[15] +'", "whole_blood_donation": "' + data[16] +'", "platelet_donation": "' + data[17] +'", "plasma_donation": "' + data[18] +'", "platelet_donation": "' + data[17]'}'
+    FOUT.write(str1+'\n')
     FOUT.close()
     return 0
 
