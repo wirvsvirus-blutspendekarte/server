b7bdaa1c02ac072f65c26fd9a32cb49a0b4af829
FunKuchen <45318896+FunKuchen@users.noreply.github.com>
2020-03-21 18:11:50 +0100

    data in json format
    still not quite working, list index out of range, possibly because of commas in "Bemerkungen"

--- convert2.py
+++ convert2.py
@@ -6,9 +6,10 @@ def getData():
     for string in InputData:
         arr = string.split(',')
 
-        filteredArr = [arr[0], arr[1], arr[2],arr[3],arr[4],arr[5],arr[6],arr[7],arr[8],arr[9],arr[10],arr[11],arr[12],arr[13],arr[14],arr[15],arr[16],arr[17],arr[18],arr[19]]
+        filteredArr = [arr[0], arr[1], arr[2],arr[3],arr[4],arr[5],arr[6],arr[7],arr[8],arr[9],arr[10],arr[11],arr[12],arr[13],arr[14],arr[15],arr[16],arr[17],arr[18],arr[19],arr[20],arr[21],arr[22]]
         
-        blooddata.append(filteredArr)               
+        blooddata.append(filteredArr)
+        print(arr)             
 
     blooddata.remove(blooddata[0])
     FIN.close()
