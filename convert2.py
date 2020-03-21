6781956bf7a7fb1f0d5e2a52e968874a46c90f7d
Sören Prilop <soeren@hubblr.io>
2020-03-21 18:54:04 +0100

    Check if line read from csv might be valid

--- convert2.py
+++ convert2.py
@@ -1,19 +1,17 @@
 def getData():
-    FIN = open ("D:\\ProgrammierigeProgramme\\GitRepositories\\blutspendekarte\\server\\b.csv", "r")
+    FIN = open ("b.csv", "r")
 
     InputData = FIN.read().split('\n')
     blooddata = []
     for string in InputData:
         arr = string.split(',')
-
-        filteredArr = [arr[0], arr[1], arr[2],arr[3],arr[4],arr[5],arr[6],arr[7],arr[8],arr[9],arr[10],arr[11],arr[12],arr[13],arr[14],arr[15],arr[16],arr[17],arr[18],arr[19],arr[20],arr[21],arr[22]]
-        
-        blooddata.append(filteredArr)
-        print(arr)             
+        if (len(arr) > 22):
+            filteredArr = [arr[0], arr[1], arr[2],arr[3],arr[4],arr[5],arr[6],arr[7],arr[8],arr[9],arr[10],arr[11],arr[12],arr[13],arr[14],arr[15],arr[16],arr[17],arr[18],arr[19],arr[20],arr[21],arr[22]]
+            blooddata.append(filteredArr)
 
     blooddata.remove(blooddata[0])
     FIN.close()
 
     return blooddata
 
-print(getData())
+getData()
