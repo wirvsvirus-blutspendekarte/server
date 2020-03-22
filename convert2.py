d8f67aab437f2a41d757fb7dc5cea5b1d5d2ef6b
FunKuchen <45318896+FunKuchen@users.noreply.github.com>
2020-03-22 14:02:27 +0100

    automatically updating json file

--- convert2.py
+++ convert2.py
@@ -1,8 +1,12 @@
 import json
+import requests
 
 def getData():
-    FIN = open ("b.csv", "r")
+    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTbxaxdxBH9aYfQgsi7diiIJ-OBdIWo63BXlXOkv-hZiw_0H8cr2Ko0VDUAaH0TdpHOKWYz1pv9uloJ/pub?gid=0&single=true&output=csv'
+    req = requests.get(url, allow_redirects=True)
+    open ("b.csv", "wb").write(req.content)
     FOUT = open("output.json", "w")
+    FIN = open("b.csv", "r")
 
     InputData = FIN.read().split('\n')
     blooddata = []
@@ -30,8 +34,7 @@ def getData():
                     'plasma_donation': arr[20],
                     'erythrocyte_donation': arr[21],
                 }))
-        
-            #name = ["name: " , arr[1]] , "address: "+arr[5],"zip: "+arr[6],"city: "+arr[7],"lat: "+arr[2],"lon: "+arr[3],"mon: "+arr[8],"tue: "+arr[9],"wed: "+arr[10],"thu: "+arr[11],"fri: "+arr[12],"sat: "+arr[13],"sun: "+arr[14],"url: "+arr[15],"phone: "+arr[17],"whole_blood_donation: "+arr[18],"platelet_donation: "+arr[19],"plasma_donation: "+arr[20],"erythrocyte_donation: "+arr[21]]
+            
     blooddata.remove(blooddata[0])
     json.dump(blooddata ,FOUT, indent = 4)
     FIN.close()
