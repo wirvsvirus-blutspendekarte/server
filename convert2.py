914b3b36cabef6cecb0fc41f10093b88bb658884
FunKuchen <45318896+FunKuchen@users.noreply.github.com>
2020-03-22 01:06:51 +0100

    almost ok
    removed output.py because it is now unneccessary. Saved data as json, but it's not perfect.

--- convert2.py
+++ convert2.py
@@ -1,16 +1,23 @@
+import json
+
 def getData():
     FIN = open ("b.csv", "r")
+    FOUT = open("output.json", "w")
 
     InputData = FIN.read().split('\n')
     blooddata = []
     for string in InputData:
         arr = string.split(',')
         if (len(arr) > 22):
-            filteredArr = [arr[0], arr[1], arr[2],arr[3],arr[4],arr[5],arr[6],arr[7],arr[8],arr[9],arr[10],arr[11],arr[12],arr[13],arr[14],arr[15],arr[16],arr[17],arr[18],arr[19],arr[20],arr[21],arr[22]]
+            filteredArr = ["name: "+arr[1], "address: "+arr[5],"zip: "+arr[6],"city: "+arr[7],"lat: "+arr[2],"lon: "+arr[3],"mon: "+arr[8],"tue: "+arr[9],"wed: "+arr[10],"thu: "+arr[11],"fri: "+arr[12],"sat: "+arr[13],"sun: "+arr[14],"url: "+arr[15],"phone: "+arr[17],"whole_blood_donation: "+arr[18],"platelet_donation: "+arr[19],"plasma_donation: "+arr[20],"erythrocyte_donation: "+arr[21]]
             blooddata.append(filteredArr)
 
     blooddata.remove(blooddata[0])
+    json.dump(blooddata ,FOUT, indent = 4)
+    #FOUT.write(str(json.dumps(blooddata)) + '\n')
+    #print(json.dumps(blooddata))
     FIN.close()
+    FOUT.close()
 
     return blooddata
 
