889f0c716f6c42fbb4026d1427a0b9be5ecbeaa8
Daniëlle <danielle@schuman.de>
2020-03-22 13:13:41 +0100

    Fixed database.py

--- database.py
+++ database.py
@@ -1,40 +1,44 @@
 import psycopg2
 import sys
 import json
-import subprocess
 
-
-subprocess.call(['./create_database.sh'])
 
 con = None
 
 try:
-    con = psycopg2.connect("host='localhost' dbname='blutspendekartedb' user='postgres' password=''")
+    con = psycopg2.connect("host='blutspendekarte.de' dbname='blutspendekarte' user='blutspendekarte' password='IchBin1Alpaka'")
     cur = con.cursor()
-    cur.execute("CREATE TABLE Blutspendezentren (Id INTEGER PRIMARY KEY, name VARCHAR(50), address VARCHAR(100), zip VARCHAR(5), city VARCHAR(50), lat FLOAT, lon FLOAT, mon VARCHAR(20), tue VARCHAR(20), wed VARCHAR(20), thu VARCHAR(20), fri VARCHAR(20), sat VARCHAR(20), sun VARCHAR(20), url VARCHAR(512), phone VARCHAR(25), whole_blood_donation VARCHAR(4), platelet_donation VARCHAR(4), plasma_donation VARCHAR(4), erythrocytes_donation VARCHAR(4))")
+    cur.execute("CREATE TABLE Blutspendezentren (Id INTEGER PRIMARY KEY, name VARCHAR(100), address VARCHAR(100), zip VARCHAR(7), city VARCHAR(100), lat VARCHAR(100), lon VARCHAR(100), mon VARCHAR(100), tue VARCHAR(100), wed VARCHAR(100), thu VARCHAR(100), fri VARCHAR(100), sat VARCHAR(100), sun VARCHAR(100), url VARCHAR(512), phone VARCHAR(100), whole_blood_donation VARCHAR(100), platelet_donation VARCHAR(100), plasma_donation VARCHAR(100), erythrocytes_donation VARCHAR(100))")
 
     importfile = open("output.json", "r")
-    importdict = json.loads(importfile.read())
+    importlist = json.loads(importfile.read())
     importfile.close()
 
     i = 0
 
-    for j in importdict[TODO]:
-        cur.execute("INSERT INTO Blutspendezentren VALUES(i, j['name'], j['address'], j['zip'], j['city'], j['lat'], j['lon'], j['timings']['mon'], j['timings']['tue'], j['timings']['wed'], , j['timings']['thu'], j['timings']['fri'], j['timings']['sat'], j['timings']['sun'], j['url'], j['phone'], j['whole_blood_donation'], j['platelet_donation'], j['plasma_donation'], j['erythrocytes_donation'])")
+    for j in importlist[1:]:
+        sql_string = 'INSERT INTO Blutspendezentren VALUES (' + str(i)
+        for key in j:
+            if not j[key] == '':
+                sql_string = sql_string + ', \'' + j[key] + '\''
+            else:
+                sql_string = sql_string + ', NULL'
+        sql_string = sql_string + ' )'
+        # cur.execute("INSERT INTO Blutspendezentren VALUES (" + i + ", " + j['name'] + ", " + j['address'] + ", " + j['zip'] + ", " + j['city'], j['lat'], j['lon'], j['mon'], j['tue'], j['wed'], j['thu'], j['fri'], j['sat'], j['sun'], j['url'], j['phone'], j['whole_blood_donation'], j['platelet_donation'], j['plasma_donation'], j['erythrocytes_donation'])")
+        cur.execute(sql_string)
         i += 1
 
-    cur.execute("CREATE TABLE Spender (Id INTEGER PRIMARY KEY, vorname VARCHAR(50), nachname VARCHAR(50), email VARCHAR(100), handy VARCHAR(25), anmerkungen VARCHAR(max))")
-    cur.execute("CREATE TABLE Termin (Id INTEGER PRIMARY KEY, Blutspendezentrum INTEGER REFERENCES Blutspendezentren(id), Spender INTEGER REFERENCES Spender(Id), Datum DATE, Uhrzeit TIME, Kategorie VARCHAR(12), Bestaetigt VARCHAR(4), Anmerkungen VARCHAR(max))")
+    cur.execute("CREATE TABLE Spender (Id INTEGER PRIMARY KEY, vorname VARCHAR(50), nachname VARCHAR(50), email VARCHAR(100), handy VARCHAR(25), anmerkungen VARCHAR)")
+    cur.execute("CREATE TABLE Termin (Id INTEGER PRIMARY KEY, Blutspendezentrum INTEGER REFERENCES Blutspendezentren(id), Spender INTEGER REFERENCES Spender(Id), Datum DATE, Uhrzeit TIME, Kategorie VARCHAR(12), Bestaetigt VARCHAR(4), Anmerkungen VARCHAR)")
     # Kategorie = Wunsch/ Alternativ 1/ Alternativ 2; Bestaetigt = Ja / Nein
-    
+
     con.commit()
 
 except psycopg2.DatabaseError as e:
     if con:
         con.rollback()
 
-    print
-    'Error %s' % e
+    print('Error %s' % e)
     sys.exit(1)
 
 finally:
