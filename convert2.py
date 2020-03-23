444a9074e9d041c3b59244ed97ecd8e031e894c8
Sören Prilop <soeren@hubblr.io>
2020-03-23 22:45:48 +0100

    Load data directly into database

--- convert2.py
+++ convert2.py
@@ -1,45 +1,60 @@
 import json
+from io import StringIO
+
 import requests
+import csv
+import db
 
 def getData():
+    connection = db.get_connection()
+    cursor = connection.cursor()
+
     url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTbxaxdxBH9aYfQgsi7diiIJ-OBdIWo63BXlXOkv-hZiw_0H8cr2Ko0VDUAaH0TdpHOKWYz1pv9uloJ/pub?gid=0&single=true&output=csv'
-    req = requests.get(url, allow_redirects=True)
-    open ("b.csv", "wb").write(req.content)
-    FOUT = open("output.json", "w")
-    FIN = open("b.csv", "r")
+    r = requests.get(url, allow_redirects=True)
+    r.encoding = 'utf-8'
 
-    InputData = FIN.read().split('\n')
-    blooddata = []
-    for string in InputData:
-        arr = string.split(',')
-        if (len(arr) > 22):
-            blooddata.append(( {
-                    'name': arr[1],
-                    'address': arr[5],
-                    'zip': arr[6],
-                    'city': arr[7],
-                    'lat': arr[2],
-                    'lon': arr[3],
-                    'mon': arr[8],
-                    'tue': arr[9],
-                    'wed': arr[10],
-                    'thu': arr[11],
-                    'fri': arr[12],
-                    'sat': arr[13],
-                    'sun': arr[14],
-                    'url': arr[15],
-                    'phone': arr[17],
-                    'whole_blood_donation': arr[18],
-                    'platelet_donation': arr[19],
-                    'plasma_donation': arr[20],
-                    'erythrocyte_donation': arr[21],
-                }))
-            
-    blooddata.remove(blooddata[0])
-    json.dump(blooddata ,FOUT, indent = 4)
-    FIN.close()
-    FOUT.close()
+    mapping = {
+        'name': (lambda row: row.get('Name')),
+        'address': (lambda row: row.get('Adresse')),
+        'zip': (lambda row: row.get('PLZ')),
+        'city': (lambda row: row.get('Ort')),
+        'lat': (lambda row: row.get('Latitude')),
+        'lon': (lambda row: row.get('Longitude')),
+        'mon': (lambda row: row.get('Mo')),
+        'tue': (lambda row: row.get('Di')),
+        'wed': (lambda row: row.get('Mi')),
+        'thu': (lambda row: row.get('Do')),
+        'fri': (lambda row: row.get('Fr')),
+        'sat': (lambda row: row.get('Sa')),
+        'sun': (lambda row: row.get('So')),
+        'url': (lambda row: row.get('Website')),
+        'phone': (lambda row: row.get('Telefon')),
+        'whole_blood_donation': (lambda row: row.get('Vollblutspende')),
+        'platelet_donation': (lambda row: row.get('Thrombozytenspende')),
+        'plasma_donation': (lambda row: row.get('Plasmaspende')),
+        'erythrocyte_donation': (lambda row: row.get('Erythrozyten'))
+    }
 
-    return blooddata
+    reader = csv.DictReader(StringIO(r.text), delimiter=',')
+    for row in reader:
+        keys = []
+        values = []
+        placeholders = []
+
+        for key, value in mapping.items():
+            keys.append(key)
+            placeholders.append('%s')
+            values.append(value(row))
+
+        joinedKeys = ", ".join(keys)
+        joinedPlaceholders = ", ".join(placeholders)
+
+        cursor.execute(
+            "insert into blutspendezentren (%s) values (%s)" % (joinedKeys, joinedPlaceholders),
+            values
+        )
+
+    connection.commit()
 
 getData()
+
