182fc328597ac9668ae43051d1844cf9c377dd40
Daniëlle <danielle@schuman.de>
2020-05-17 16:09:05 +0200

    Added methods for editing, deleting and mapping to JSON to routes/wartezeiten

--- routes/wartezeit.py
+++ routes/wartezeit.py
@@ -10,6 +10,15 @@ from models.wartezeit import Wartezeit
 blueprint = Blueprint('wartezeit', __name__)
 
 
+def map_to_dict(wartezeit):
+    return {
+        "id": str(wartezeit.id),
+        "einrichtung": wartezeit.einrichtung,
+        "wartezeit": wartezeit.wartezeit,
+        "meldezeit": wartezeit.meldezeit
+    }
+
+
 @blueprint.route('', methods=('POST',))
 def create():
     wartezeit = Wartezeit()
@@ -18,5 +27,23 @@ def create():
     wartezeit.wartezeit = int(request.form['wartezeit'])
     wartezeit.meldezeit = datetime.now()
     wartezeit.save(force_insert=True)
+
+    return json.dumps({"success": True})
+
+
+@blueprint.route('edit', methods=['POST'])
+def edit():
+    wartezeit = Wartezeit.get_by_id(request.form['id'])
+    wartezeit.wartezeit = int(request.form['wartezeit'])
+    wartezeit.meldezeit = datetime.now()
+    wartezeit.save()
+
+    return json.dumps({"success": True})
+
+
+@blueprint.route('delete', methods=['POST'])
+def delete():
+    wartezeit = Wartezeit.get_by_id(request.form["id"])
+    wartezeit.delete_instance()
 
     return json.dumps({"success": True})
